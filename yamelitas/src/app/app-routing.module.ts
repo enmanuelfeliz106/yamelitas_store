23f181a91146b4f3cbefd11c988dffd211b89e96
Enmanuel Feliz Espinal <enmanuelfeliz106@gmail.com>
2020-12-28 15:24:47 -0600

    SideBar y main section
    Solo el main esta linkeado a firebase

--- yamelitas/src/app/app-routing.module.ts
+++ yamelitas/src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
 import { ProductosComponent } from './productos/productos.component';
 
-const routes: Routes = [{path: "inicio", component:InicioComponent, pathMatch: "full"},
+const routes: Routes = [{path: "", redirectTo: "inicio", pathMatch: "full"},
+                        {path: "inicio", component:InicioComponent, pathMatch: "full"},
                         {path: "productos", component:ProductosComponent, pathMatch: "full"}];
 
 @NgModule({
--- yamelitas/src/app/inicio/inicio.component.ts
+++ yamelitas/src/app/inicio/inicio.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
+
+interface publicacion{
+  titulo: string;
+  imagen_portada: string;
+  contexto: string;
+  contexto_desarrollo: string;
+  productos_titulo: string;
+  productos_desarrollo: string;
+  productos_productos: Array<string>;
+  hashtags: Array<string>;
+
+}
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.scss']
 })
+
+
 export class InicioComponent implements OnInit {
+
+  publicaciones: Array<object> = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    
+    firebase.default.firestore().collection('publicaciones').get()
+    .then((snapshot) => {
+      snapshot.forEach((doc) => {
+        this.publicaciones.push(doc.data());
+        
+        console.log(doc.id, '=>', doc.data());
+      });
+    })
+    .catch((err) => {
+      console.log('Error getting documents', err);
+    });
+   
+
   }
+
 
 }
